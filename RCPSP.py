7690803c0c6308b8ce623b9928a6f71aedd0404e
egbertocaetano <egbertocaetano@gmail.com>
2015-11-29 17:11:15 -0200

    Adição da funçao selectElegibleActivities(Sg, Dg, tp)

--- RCPSP.py
+++ RCPSP.py
@@ -3,8 +3,10 @@ import string
 
 offsetIni = 18
 offsetFim = 32 + 4
+offsetResource = 89
 numTasks = 32
 tasks = []
+resources = []
 
 #Limpa a linha e a coloca em lista
 def clearLine(e):
@@ -36,7 +38,6 @@ def clearLine(e):
 
 	return v
 
-
 #Carregando arquivo alimentar o algortimo
 def load (nome_arq):
 	
@@ -63,9 +64,14 @@ def load (nome_arq):
 
 		nt = nt + 1
 
-	geraPrecedentes()	
+	#Capturando a quantidade de recursos	
+	e3 = lines[offsetResource]
 
-def ehPredecessor(t, pt):
+	resources = clearLine(e3)
+
+	generatePredecessors()	
+
+def isPredecessor(t, pt):
 	
 	try:
 		i = pt[1].index(t[0])
@@ -75,18 +81,107 @@ def ehPredecessor(t, pt):
 		return 1
 
 #Cria a lista de predecessores
-def geraPrecedentes():
+def generatePredecessors():
 
 	pred = []
 	i = 0
 	while i < numTasks:
 		
 		for t2 in tasks:
-			if ehPredecessor(tasks[i], t2) == 1:
+			if isPredecessor(tasks[i], t2) == 1:
 				pred.append(t2[0])
 		tasks[i].append(pred)
 		pred = []		
 		i = i + 1
+
+
+def selectElegibleActivities(s, d, t):
+
+	length = len(t) 
+	count = 0
+	listpop = []
+
+	#iterando sobre conjunto de atividades a serem processadas
+	while count < length: #for a in t:
+		cp = 0
+		#iterando sobre conjunto de predecedentes da atividades 
+		for p in t[count][4]:
+			try:
+				i = s.index(p) # Tenta recuperar a posição da atividade predecedente já escalonada
+			except ValueError:
+				break
+			else:
+				cp = cp + 1
+		#Verficando se a atividade tornou-se elegivel. Caso seja, adiciona ao conjunto d		
+		if cp == len(t[count][4]):
+			d.append(t[count]) #Inserindo na lista de elegiveis
+			listpop.append(t[count]) # Quardando referencia do objeto para excluir posteriormento
+
+		count = count + 1
+	
+	#Removendo atividades da lista de atividades a serem processadas
+	for pop in listpop:
+		try:
+			i = t.index(pop)
+			t.pop(i)			
+		except ValueError:
+			print "Erro: Ocorreu um erro ao tentar excluir um elemento da lista de atividades a serem processadas"	
+		
+	
+
+
+	#return d	
+
+#Serial Schedule Generation Scheme
+def SGS():
+
+	tp = tasks #Conjunto de tarefas para serem processadas
+	Dg = [] #Conjuto de atividades a serem escolhidas
+	Sg = [] #Conjuto de atividades escolhida
+	Rk = [] #Conjuto de recursos
+	F = 0	#Custo da solução gerada
+	g = len(tasks) #Quantidade de ativdades do projeto
+	st = [g] #Starting time (Tempo de início da atividade)
+	et = [g] #Ending Time (Tempo de términio) 
+	RU = [len(resources)]
+
+	#Processando a primmeira atividade
+	task = tp.pop(0)
+	st[0] = et = 0
+	Sg.append(task[0])
+
+	#ISSO ESTÁ ERRADO! Inserindo sucessores para o vetor de atividades a serem escolhidas
+	#Dg = task[1]
+
+	selectElegibleActivities(Sg, Dg, tp)
+	print Dg
+	print "\n"
+	for i in tp:
+		print i
+
+	'''
+	i = 1
+	while i < g:
+
+		selectElegibleActivities(Sg, Dg, tp)
+		print Dg
+		i = i + 1
+		#Fazer uma função de seleção de atividades elegiveis
+	'''	
+
+
+	
+
+
+
+
+
+
+
+
+
+
+#Início da execução do código
 
 
 
@@ -94,3 +189,8 @@ load('teste.sm')
 
 for i in tasks:
 	print i
+print "\n"
+SGS()
+
+#for i in tasks:
+#	print i
