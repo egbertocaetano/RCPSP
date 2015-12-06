792da78f394526efd64b776e34e03a7bbedea3bb
egbertocaetano <egbertocaetano@gmail.com>
2015-12-06 17:29:48 -0200

    Falta mutação, iniciando a função de avaliação

--- RCPSP.py
+++ RCPSP.py
@@ -287,11 +287,26 @@ def generatePopulation(n):
 
 	popu = []
 	i = 0
+	individual = {}
 
 	while i < n:
-		popu.append(SGS())
-		i = i + 1
+		individual = SGS()
+		if exsistsChromossome(individual, popu) == 1:#Verifica se o cromossomo já existe 
+			print 'Chromosome igual'
+			individual = {}
+		else:	
+			popu.append(SGS())
+			i = i + 1
 	return popu
+
+#Função para garantir que não existirá cromossomos iguais na população 
+def exsistsChromossome(chromosome, population):
+	
+	for ind in population:
+		if chromosome['Chromosome'] == ind['Chromosome']: 
+			return 1
+
+	return -1 		
 
 #Ordena a população pelo custo menor tempo de execução
 def generatepopulationElitist(pop):
@@ -309,53 +324,151 @@ def selectsFit(pop, txSlection):
 
 	return elit
 
-def crossOverOnePoint(ind1, ind2, nG):
+
+def crossOver(ind1, ind2, nG, qp):
+
 
 	pointer = 0
-	son1 = []
-	son2 = []
+	threshold = 0
+	chromossome1 = []
+	chromossome2 = []
+	son1 = {}
+	son2 = {}
 	lenInd1 = len(ind1['Chromosome']) 
 	lenInd2 = len(ind2['Chromosome'])
 	listrandom = []
+	offset = lenInd1/qp
+	residual = lenInd1%qp
+	set = 0
+
+	#listrandom = range(0,lenInd1)
+	#pointer =  random.choice(listrandom)
 
 	if lenInd1 == lenInd2:
-
-		listrandom = range(0,lenInd1)
-
-		pointer =  random.choice(listrandom)
-
-		print pointer
-
 		#gerando filhos
-		son1 = ind1['Chromosome'][0:pointer] + ind2['Chromosome'][pointer:lenInd2]
-		son2  = ind2['Chromosome'][0:pointer] + ind1['Chromosome'][pointer:lenInd1]
+		for i in range(1,qp+1):
+			#print 'i: ', i, 'offset: ', offset, 'pointer: ' , pointer 
+			threshold = (i*offset)
+
+			if (i%2) == 1:
+				chromossome1 += ind1['Chromosome'][pointer:threshold]
+				chromossome2 += ind2['Chromosome'][pointer:threshold]
+				pointer = threshold
+			else:
+				chromossome1 += ind2['Chromosome'][pointer:threshold]
+				chromossome2 += ind1['Chromosome'][pointer:threshold]
+				pointer = threshold
+		
+		if residual > 0:
+			if ((threshold + residual)%2) == 1:
+				chromossome1 += ind1['Chromosome'][pointer:threshold+residual]
+				chromossome2 += ind2['Chromosome'][pointer:threshold+residual]
+			else:
+				chromossome1 += ind2['Chromosome'][pointer:threshold+residual]
+				chromossome2 += ind1['Chromosome'][pointer:threshold+residual]
 	else:
 		print 'Pais com tamanho de gene diferente!'
 		exit(1)
 
+	son1['Chromosome'] = chromossome1	
+	son2['Chromosome'] = chromossome2
 
 	nG.append(son1)
 	nG.append(son2)	
 
 
+#def mutation(chromosome, probability):
+'''
+def isRepeatedGene(chromosome):
+
+	for gene in c
+
+def evaluationIndividual(individual):
+
+	tp = [] #Conjunto de tarefas para serem processadas
+	#Dg = [] #Conjuto de atividades a serem escolhidas
+	Sg = [] #Conjuto de atividades escolhida
+	Rkt = [] #Quantidade de disponíveis recursos em tempo t
+	F = []	#Tempo fim das atividades
+	g = len(tasks) #Quantidade de ativdades do projeto
+	et = [] #Ending Time (Tempo de términio)
+	#etc = []
+	resourceUsageTime = {} #Timeline de uso dos recursos
+	individual = {} #Individuo gerado na execuç
+
+	tp = copy.deepcopy(tasks)
+	
+	
+	#Processando a primmeira atividade
+	task = tp.pop(0)
+	et.append({'NR':task['NR'], 'TimeEnd':0})
+	#etc.append(0)
+	Sg.append(task['NR'])
+	F.append(1)
+	resourceUsageTime[0] ={'R1' : 0, 'R2' : 0, 'R3' : 0, 'R4' : 0} #Inserindo uso dos recursos no instante 0
+
+
+	i = 1
+	while i < g:
+
+		#função de seleção de atividades elegiveis
+		selectElegibleActivities(Sg, Dg, tp)
+		#Seleciona uma atividade de forma randomica
+		j = getRandomElegibleActivitie(Dg)
+		
+		#Criar a função de consumo de resources	
+		
+		#Determinando o tempo de fim mais cedo da atividade j, ignorando a disponibilidade de recursos 
+		#etc.append({'NR': j['NR'], 'TimeMaxPredecessor': getEarliestEndingPredecessor(j, et)})
+
+		ES = getEarliestEndingPredecessor(j, et)
+
+		#Adcionando o tempo fim da execução atividade j e atualização de recursos. 
+		minJ = allocatingActivityOnTimeLine(j,resourceUsageTime,ES)
+
+		
+		et.append({'NR':j['NR'], 'TimeEnd': minJ}) 
+
+		#Calculando o tempo final do conjunto
+		F.append(et[-1])
+
+		#Inserindo na lista de atividades já escalonadas	
+		Sg.append(j['NR'])
+
+		i = i + 1
+
+
+def evaluationNewGeneration(newGeneration):
+
+'''
+
+
+
+#############################################################################################################################################
 #Início da execução do código
 load('teste.sm')
 
 #Incio do algoritmo genético
-population = generatePopulation(10)
-elit = selectsFit(population, 45)
+numberpopulation = int(sys.argv[1])
+numberpoints = int(sys.argv[2])
+txSlection = int(sys.argv[3])
+
+population = generatePopulation(numberpopulation)
+elit = selectsFit(population, txSlection)
 newGeneration = []
 
+'''
 print 'Melhores pais:'
 for ind in elit:
 	print ind
 
-crossOverOnePoint(elit[0], elit[1], newGeneration)
+#crossOverOnePoint(elit[0], elit[1], newGeneration)
+crossOver(elit[0], elit[1], newGeneration, numberpoints)
 
 print 'Melhores filhos:'
 for ind in newGeneration:
 	print ind
-
+'''
 
 
 
